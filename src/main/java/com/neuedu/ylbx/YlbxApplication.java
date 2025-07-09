c46aceb186cd2862a0b0275f3dd06c3ff83643b8
苏东煌 <3086783578@qq.com>
2025-07-09 15:06:40 +0800

    初始版本

---
+++ src/main/java/com/neuedu/ylbx/YlbxApplication.java
@@ -0,0 +1,13 @@
+package com.neuedu.ylbx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlbxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlbxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neuedu/ylbx/YlbxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuedu.ylbx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlbxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
